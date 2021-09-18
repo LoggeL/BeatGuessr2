27bed64ba03bbc301ccbd967e181329a80530bcb
LoggeL <hyper.xjo@gmx.de>
2021-09-18 12:49:39 +0200

    remove ws from landing

--- client/js/landing.js
+++ client/js/landing.js
@@ -1,41 +1,22 @@
-const socket = io({
-    transports: ['websocket']
-})
-
 const ping = document.getElementById('ping')
 const playerName = document.getElementById('playerName')
 const playerTeam = document.getElementById('playerTeam')
 const submitButton = document.getElementById('submitButton')
 
-socket.on('connect', () => {
-    console.log('connected', socket.id)
-    setInterval(() => {
-        socket.emit('ping', Date.now())
-    }, 1000)
+playerName.addEventListener('keyup', readyCheck)
+playerTeam.addEventListener('change', readyCheck)
 
-    socket.on('pong', timestamp => {
-        ping.innerText = `${Math.round((Date.now() - timestamp) * .5)} ms`
-    })
-
-    playerName.addEventListener('keyup', readyCheck)
-    playerTeam.addEventListener('change', readyCheck)
-
-    function readyCheck() {
-        if (playerName.value && playerTeam.value) {
-            submitButton.disabled = false
-        } else {
-            submitButton.disabled = true
-        }
+function readyCheck() {
+    if (playerName.value && playerTeam.value) {
+        submitButton.disabled = false
+    } else {
+        submitButton.disabled = true
     }
+}
 
-    submitButton.addEventListener('click', () => {
-        if (submitButton.disabled) return
-        localStorage.setItem('playerName', playerName.value)
-        localStorage.setItem('playerTeam', playerTeam.value)
-        window.location.href = '/game.html'
-    })
-})
-
-socket.on('disconnect', () => {
-    console.log('disconnected')
+submitButton.addEventListener('click', () => {
+    if (submitButton.disabled) return
+    localStorage.setItem('playerName', playerName.value)
+    localStorage.setItem('playerTeam', playerTeam.value)
+    window.location.href = '/game.html'
 })
